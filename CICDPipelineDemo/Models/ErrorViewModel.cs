183a2123b96d4cb1ed2db4f6132cb12f3e371a49
Meet Dobariya <mm.meetd@gmail.com>
2024-05-31 17:04:24 +0530

    Initial commit.

---
+++ CICDPipelineDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CICDPipelineDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
